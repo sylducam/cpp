78f7a84a514f852b91ba3304fb197aecc9109d7d
Sylvain Ducamp <sylducam@z4r9p7.42lyon.fr>
2021-12-30 19:57:07 +0100

    video on classes

--- 0/ex00/class_tests.cpp
+++ 0/ex00/class_tests.cpp
@@ -1,26 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   stdio_tests.cpp                                    :+:      :+:    :+:   */
+/*   class_tests.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sylducam <sylducam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/28 19:59:56 by sylducam          #+#    #+#             */
-/*   Updated: 2021/12/30 18:48:52 by sylducam         ###   ########.fr       */
+/*   Created: 2021/12/30 18:53:37 by sylducam          #+#    #+#             */
+/*   Updated: 2021/12/30 18:56:34 by sylducam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-
-int	main(void)
-{
-	char	buff[512];
-
-	std::cout << "Hello World !" << std::endl;
-	
-	std::cout << "Type a word: ";
-	std::cin >> buff;
-	std::cout << "You typed: [" << buff << "]" << std::endl;
-
-	return(0);
-}
--- 0/ex00/header_class_tests.hpp
+++ 0/ex00/header_class_tests.hpp
@@ -1,26 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   stdio_tests.cpp                                    :+:      :+:    :+:   */
+/*   header_class_tests.hpp                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sylducam <sylducam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/28 19:59:56 by sylducam          #+#    #+#             */
-/*   Updated: 2021/12/30 18:48:52 by sylducam         ###   ########.fr       */
+/*   Created: 2021/12/30 18:54:47 by sylducam          #+#    #+#             */
+/*   Updated: 2021/12/30 18:56:19 by sylducam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-
-int	main(void)
-{
-	char	buff[512];
-
-	std::cout << "Hello World !" << std::endl;
-	
-	std::cout << "Type a word: ";
-	std::cin >> buff;
-	std::cout << "You typed: [" << buff << "]" << std::endl;
-
-	return(0);
-}
