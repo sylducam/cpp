9673751ad78f9c477c450c88aad2802b1521c55e
Sylvain <sylducam@gmail.com>
2022-01-01 15:28:36 +0100

    tidied a little bit

--- 0/ex00/classes/Sample.class.hpp
+++ 0/ex00/classes/Sample.class.hpp
@@ -1,13 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   class_tests.cpp                                    :+:      :+:    :+:   */
+/*   Sample.class.hpp                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sylducam <sylducam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/30 18:53:37 by sylducam          #+#    #+#             */
-/*   Updated: 2021/12/30 18:56:34 by sylducam         ###   ########.fr       */
+/*   Created: 2021/12/30 18:54:47 by sylducam          #+#    #+#             */
+/*   Updated: 2021/12/30 18:56:19 by sylducam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#ifndef SAMPLE_CLASS_HPP
+# define SAMPLE_CLASS_HPP
+
+class Sample
+{
+    public:
+
+        Sample(void); // contructeur = nom de la classe
+        ~Sample(void); // destructeur = ~nom de la classe
+};
+
+#endif
--- 0/ex00/classes/class_tests.cpp
+++ 0/ex00/classes/class_tests.cpp
@@ -1,12 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   header_class_tests.hpp                             :+:      :+:    :+:   */
+/*   class_tests.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sylducam <sylducam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/30 18:54:47 by sylducam          #+#    #+#             */
-/*   Updated: 2021/12/30 18:56:19 by sylducam         ###   ########.fr       */
+/*   Created: 2021/12/30 18:53:37 by sylducam          #+#    #+#             */
+/*   Updated: 2021/12/30 18:56:34 by sylducam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "Sample.class.hpp"
+
+Sample::Sample(void)
+{
+    std::cout << "Contructor called" << std::endl;
+    return;
+}
+
+Sample::~Sample(void)
+{
+    std::cout << "Destructor called" << std::endl;
+    return;
+}
