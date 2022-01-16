09f6cb6f2fa86426aca6edc4d04abbbe9172d0b7
Sylvain Ducamp <sylducam@z4r7p7.42lyon.fr>
2022-01-16 10:48:19 +0100

    l

--- 0/ex01/my_phone_book.cpp
+++ 0/ex01/my_phone_book.cpp
@@ -1,1 +1,9 @@
-i
+#include <string>
+#include <istream>
+#include "my_phone_book.hpp"
+
+int	main(int argc, char **argv)
+{
+
+	return (0);
+}
---
+++ 0/ex01/my_phone_book.hpp
@@ -0,0 +1,8 @@
+#ifndef MY_PHONE_BOOK_HPP
+# define MY_PHONE_BOOK_HPP
+#include <string>
+#include <istream>
+
+
+
+#endif
