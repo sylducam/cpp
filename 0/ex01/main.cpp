a9b2e90920ede157d78e4a3fa2c111b4dbf23238
Sylvain Ducamp <sylducam@gmail.com>
2022-01-17 17:36:15 +0100

    prebreak save

--- 0/ex01/main.cpp
+++ 0/ex01/main.cpp
@@ -1,7 +1,24 @@
 // ajoutes les includes
+#include "my_phone_book.class.hpp"
+fonction ADD
+	phone_book	instance;
+
+
 int main()
 {
-	start	instance;
+	std::string name;
+	std::cout << "Welcome to your phone book" << std::endl;
+	std::cout << "Type what you want to do : ADD / SEARCH / EXIT" << std::endl;
+	std::getline(std::cin, name);
+	if (name == "ADD")
+		std::cout << "X" << std::endl; // fonction a la place
+	else if (name == "SEARCH")
+		std::cout << "Y" << std::endl; // fonction a la place
+	else if (name == "EXIT")
+		std::cout << "Z" << std::endl; // fonction a la place
+									   // removes all contacts and quit the prgm
+	else
+		std::cout << "Wrong entry" << std::endl; // retour d'erreur
 	// 4 - Une fonction dans la classe permet de recuperer lequel des trois
 	// et de la, lances l'une des trois actions differente. (je suis brouillon
 	// a partir de la car je ne sais pas ce que je vais faire. Besoin de lire
--- 0/ex01/my_phone_book.class.cpp
+++ 0/ex01/my_phone_book.class.cpp
@@ -2,24 +2,12 @@
 
 phone_book::phone_book(void)
 {
-	// fous a peu pres totu ce qui suit dans le main
-	std::string name;
-	std::cout << "Welcome to your phone book" << std::endl;
-	std::cout << "Type what you want to do : ADD / SEARCH / EXIT" << std::endl;
-	std::getline(std::cin, name);
-	if (name == "ADD")
-		std::cout << "X" << std::endl; // fonction a la place
-	else if (name == "SEARCH")
-		std::cout << "Y" << std::endl; // fonction a la place
-	else if (name == "EXIT")
-		std::cout << "Z" << std::endl; // fonction a la place
-									   // removes all contacts and quit the prgm
-	else
-		std::cout << "Wrong entry" << std::endl; // retour d'erreur
-
+	std::cout << "Contructor called" << std::endl; // del later. just so you know it works
+	return;
 }
 
 phone_book::~phone_book(void)
 {
+    std::cout << "Destructor called" << std::endl; // del later. just so you know it works
 	return;
 }
