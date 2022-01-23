4663b27455545b98c7048393fdf5cf29174678fe
Sylvain Ducamp <sylducam@gmail.com>
2022-01-23 10:43:33 +0100

    keep on coding the functions

--- 0/ex02/Account.cpp
+++ 0/ex02/Account.cpp
@@ -21,13 +21,17 @@ Account::Account(int initial_deposit)
 	this->_nbAccounts++;
 
 	this->_displayTimestamp();
-	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";created" << std::endl;
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "created" << std::endl;
 }
 
 Account::~Account(void)
 {
 	this->_displayTimestamp();
-	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";closed" << std::endl;
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "closed" << std::endl;
 }
 
 void	Account::_displayTimestamp(void)
@@ -43,23 +47,39 @@ void	Account::_displayTimestamp(void)
 			  << std::setw(2) << localt->tm_sec << "] ";
 }
 
+int		Account::getNbAccounts(void)
+{
+	return _nbAccounts;
+}
+
+int		Account::getTotalAmount(void)
+{
+	return _totalAmount;
+}
+
+int		Account::getNbDeposits(void)
+{
+	return _totalNbDeposits;
+}
+
+int		Account::getNbWithdrawals(void)
+{
+	return _totalNbWithdrawals;
+}
+
 int		Account::checkAmount(void) const
 {
-	return this->_amount > 0; // ICI
+	return this->_amount > 0; // pourquoi > 0 ?
 }
 
 void	Account::displayStatus(void) const
 {
-	int	i = 0;
-	while(i <= getNbAccounts())
-	{
-		_displayTimestamp();
-		std::cout << "index: " << i << ";";
+		Account::_displayTimestamp(); // "Account::" needed ?
+		std::cout << "index: " << _accountIndex << ";";
 		std::cout << "amount: " << checkAmount() << ";"; // coder checkAmount et ICI
-		std::cout << "deposits: " << i << ";";
-		std::cout << "withdrawals: " << i << ";";
+		std::cout << "deposits: " << getNbDeposits() << ";";
+		std::cout << "withdrawals: " << getNbWithdrawals() << ";";
 		i--;
-	}
 }
 
 void	Account::displayAccountsInfos(void)
--- 0/ex02/Account.hpp
+++ 0/ex02/Account.hpp
@@ -23,11 +23,11 @@ public:
 
 	typedef Account		t;
 
-	static int	getNbAccounts( void );
-	static int	getTotalAmount( void );
-	static int	getNbDeposits( void );
-	static int	getNbWithdrawals( void );
-	static void	displayAccountsInfos( void );
+	static int	getNbAccounts( void ); // coded
+	static int	getTotalAmount( void ); // coded
+	static int	getNbDeposits( void ); // coded
+	static int	getNbWithdrawals( void ); // coded
+	static void	displayAccountsInfos( void ); // coded
 
 	Account( int initial_deposit );
 	~Account( void );
