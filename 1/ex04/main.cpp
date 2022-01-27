8173a292e067180c337f138520e3d7431077178d
Sylvain Ducamp <sylducam@gmail.com>
2022-01-27 18:38:12 +0100

    ex04 OK, understand it now

--- 1/ex04/main.cpp
+++ 1/ex04/main.cpp
@@ -11,20 +11,21 @@ int main(int argc, char **argv)
 	}
 	else
 	{
-		std::ifstream	my_file(argv[1]); // filtrer fichiers txt, ne pas prendre fichiers binaire (isbinary), ou image, a chercher
-		if (!my_file.is_open() || !my_file.open(my_file, std::ios::binary))
+		std::ifstream	my_file(argv[1]);
+		if (my_file.fail())
+
 		{
 			std::cout << "There's an issue with your file. Make sure that it exists and has the right rights." << std::endl;
 			return (-1);
 		}
 		if (argv[2][0] == 0)
 		{
-			std::cout << "Third argument should not be empty." << std::endl;
+			std::cout << "Second argument should not be empty." << std::endl;
 			return (-1);
 		}
 		if (argv[3][0] == 0)
 		{
-			std::cout << "Fourth argument should not be empty." << std::endl;
+			std::cout << "Third argument should not be empty." << std::endl;
 			return (-1);
 		}
 		std::string		s1 = argv[2];
@@ -44,7 +45,13 @@ int main(int argc, char **argv)
 		my_file.close();
 		temp = argv[1];
 		temp.append(".replace");
-		out_file.open(temp.c_str());
+		out_file.open(temp);
+		if (out_file.fail())
+
+		{
+			std::cout << "There's an issue with your file. Make sure that it exists and has the right rights." << std::endl;
+			return (-1);
+		}
 		out_file << res;
 		out_file.close();
 	}
