5698512d3ee4fd5c040f8e322738de80b777cd38
Sylvain Ducamp <sylducam@gmail.com>
2022-01-27 11:30:43 +0100

    ex04 on the go

---
+++ 1/ex04/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char **argv) {
+	if (argc == 4) {
+		std::ifstream	my_file(argv[1]);
+		if (argv[2][0] == 0 || argv[3][0] == 0 || !my_file.is_open())
+			return 1;
+		std::string		s1 = argv[2];
+		std::string		s2 = argv[3];
+		std::string		res;
+		size_t 			pos = 0;
+		std::string		temp((std::istreambuf_iterator<char>(my_file)), std::istreambuf_iterator<char>());
+		std::ofstream	out_file;
+
+		for (size_t n = temp.find(s1, 0); n != std::string::npos ; n = temp.find(s1, n)) {
+			res.append(temp, pos, n - pos);
+			res.append(s2);
+			pos = n + s1.size();
+			n++;
+		}
+		res.append(temp.begin() + pos, temp.end());
+		my_file.close();
+		temp = argv[1];
+		temp.append(".replace");
+		out_file.open(temp.c_str());
+		out_file << res;
+		out_file.close();
+	}
+	return 0;
+} 
