625764ae32ac2c6b6c921795946fd586651e66e2
Sylvain Ducamp <sylducam@gmail.com>
2022-01-31 11:10:46 +0100

    better 2/ex00 main

--- 2/ex00/main.cpp
+++ 2/ex00/main.cpp
@@ -11,6 +11,14 @@ int main(void)
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
-
+	
+/* added to subject's main, to test the function setRawBits
+	a.setRawBits(1);
+	b.setRawBits(2);
+	c.setRawBits(3);
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+*/
 	return (0);
 }
