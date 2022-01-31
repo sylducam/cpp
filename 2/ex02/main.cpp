b80f56d22d428a6a89e16e0dfe91a44734be5617
Sylvain Ducamp <sylducam@gmail.com>
2022-01-31 18:17:21 +0100

    2/ex02 main upgraded

--- 2/ex02/main.cpp
+++ 2/ex02/main.cpp
@@ -1,64 +1,97 @@
 #include "Fixed.hpp"
 
-int main(void) 
+int main(void)
 {
 	Fixed a;
 	Fixed const b(Fixed(5.05f) * Fixed(2));
-
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
-	std::cout << --a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a-- << std::endl;
-	std::cout << a << std::endl;
-
 	std::cout << b << std::endl;
-
 	std::cout << Fixed::max(a, b) << std::endl;
 
 
+/* added to subject's main, to test everything
+	Fixed a;
+	Fixed const b(Fixed(5.05f) * Fixed(2));
+
+	std::cout << "a = " << a << std::endl;
+	std::cout << "++a" << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << "a = " << a << std::endl;
+	std::cout << "a++" << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << "a = " << a << std::endl;
+	std::cout << "--a" << std::endl;
+	std::cout << --a << std::endl;
+	std::cout << "--a" << std::endl;
+	std::cout << --a << std::endl;
+	std::cout << "a = " << a << std::endl;
+	std::cout << "a--" << std::endl;
+	std::cout << a-- << std::endl;
+	std::cout << "a--" << std::endl;
+	std::cout << a-- << std::endl;
+	std::cout << "a--" << std::endl;
+	std::cout << "a = " << a << std::endl;
+
+	std::cout << "b = " << b << std::endl;
+
+	std::cout << "test max(a, b)" << std::endl;
+	std::cout << Fixed::max(a, b) << std::endl;
+
 	Fixed const c(Fixed(6.16f) * Fixed(3));
 
-	a = c;	
-	std::cout << a << std::endl;
+	std::cout << "a = c" << std::endl;
+	a = c;
+	std::cout << "a = " << a << std::endl;
+	std::cout << "a + b" << std::endl;
 	std::cout << a + b << std::endl;
-	std::cout << a << std::endl;
+	std::cout << "a - b" << std::endl;
 	std::cout << a - b << std::endl;
-	std::cout << a << std::endl;
+	std::cout << "a * b" << std::endl;
 	std::cout << a * b << std::endl;
-	std::cout << a << std::endl;
+	std::cout << "a / b" << std::endl;
 	std::cout << a / b << std::endl;
-	std::cout << a << std::endl;
+	std::cout << "a = " << a << std::endl;
+	std::cout << "b = " << b << std::endl;
+	std::cout << "test a > b" << std::endl;
 	if (a > b)
-		std::cout <<"a > b" << std::endl;
+		std::cout << "yes" << std::endl;
 	else
-		std::cout <<"a !< b" << std::endl;
+		std::cout << "no" << std::endl;
+	std::cout << "test a < b" << std::endl;
 	if (a < b)
-		std::cout <<"a < b" << std::endl;
+		std::cout << "yes" << std::endl;
 	else
-		std::cout <<"a !> b" << std::endl;
+		std::cout << "no" << std::endl;
+	std::cout << "a = b" << std::endl;
 	a = b;
+	std::cout << "test a <= b" << std::endl;
 	if (a <= b)
-		std::cout <<"a <= b" << std::endl;
+		std::cout << "yes" << std::endl;
 	else
-		std::cout <<"a !<= b" << std::endl;
-	std::cout << a++ << std::endl;
+		std::cout << "no" << std::endl;
+	std::cout << "a--" << std::endl;
+	std::cout << a-- << std::endl;
+	std::cout << "a = " << a << std::endl;
+	std::cout << "test a >= b" << std::endl;
 	if (a >= b)
-		std::cout <<"a >= b" << std::endl;
+		std::cout << "yes" << std::endl;
 	else
-		std::cout <<"a !>= b" << std::endl;
-	std::cout << a > b << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a < b << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a >= b << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a <= b << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a <= b << std::endl;
+		std::cout << "no" << std::endl;
+	std::cout << "test a == b" << std::endl;
+	if (a == b)
+		std::cout << "yes" << std::endl;
+	else
+		std::cout << "no" << std::endl;
+	std::cout << "test a != b" << std::endl;
+	if (a != b)
+		std::cout << "yes" << std::endl;
+	else
+		std::cout << "no" << std::endl;
+*/
 
 	return 0;
 }
