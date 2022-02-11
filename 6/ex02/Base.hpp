1b79ead1a4d30d30f69cbdb27b7c13997b483a7d
Sylvain Ducamp <sylducam@gmail.com>
2022-02-11 12:15:41 +0100

    l

--- 6/ex02/Base.hpp
+++ 6/ex02/Base.hpp
@@ -1,20 +1,9 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Base.hpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: tglory <tglory@student.42lyon.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/04 18:10:42 by tglory            #+#    #+#             */
-/*   Updated: 2022/02/06 18:24:31 by tglory           ###   ########lyon.fr   */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef BASE_HPP
 # define BASE_HPP
 
 # include <iostream>
 # include <cstdlib>
+# include <ctime>
 
 class Base {
 	public:
